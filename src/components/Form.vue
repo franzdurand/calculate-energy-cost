c88efe6d16aa313a938aa053116c70a8b32c5bc0
Franz Durand <franzdurand17@gmail.com>
2022-03-15 16:59:32 -0500

    Editable fields

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,6 +4,61 @@
       <form class="space-y-6" @submit.prevent="calculate()">
         <div>
           <label for="text" class="block text-sm font-medium text-gray-700">
+            Unit Price
+          </label>
+          <div class="mt-1">
+            <input
+              v-model="unit_price"
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+        </div>
+        <div>
+          <label for="text" class="block text-sm font-medium text-gray-700">
+            Fixed fee
+          </label>
+          <div class="mt-1">
+            <input
+              v-model="fixed_fee"
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+        </div>
+        <div>
+          <label for="text" class="block text-sm font-medium text-gray-700">
+            Maintainance
+          </label>
+          <div class="mt-1">
+            <input
+              v-model="maintainance"
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+        </div>
+        <div>
+          <label for="text" class="block text-sm font-medium text-gray-700">
+            Public light
+          </label>
+          <div class="mt-1">
+            <input
+              v-model="public_light"
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+        </div>
+        <div>
+          <label for="text" class="block text-sm font-medium text-gray-700">
+            Rural electricity
+          </label>
+          <div class="mt-1">
+            <input
+              v-model="rural_electricy"
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+        </div>
+        <div>
+          <label for="text" class="block text-sm font-medium text-gray-700">
             Last count
           </label>
           <div class="mt-1">
@@ -46,10 +101,22 @@ export default defineComponent({
   setup() {
     let last_count = ref("");
     let current_count = ref("");
+    let unit_price = ref(0.5006);
+    let fixed_fee = ref(2.61);
+    let maintainance = ref(1.32);
+    let public_light = ref(3.47);
+    let rural_electricy = ref(0.97);
 
     const calculate = () => {
       let number1 = last_count.value;
       let number2 = current_count.value;
+      
+
+      unit_price = +unit_price.value;
+      fixed_fee = +fixed_fee.value;
+      public_light = +public_light.value;
+      maintainance = +maintainance.value;
+      rural_electricy = +rural_electricy.value;
 
       let n1 = +number1;
       let n2 = +number2;
@@ -59,14 +126,8 @@ export default defineComponent({
         (isFloat(n2) || Number.isInteger(n2))
       ) {
         if (n1 > 0 && n2 > 0 && n2 > n1) {
-          const unit_price = 0.5006;
-          const fixed_fee = 2.61;
-          const meintainance = 1.32;
-          const public_light = 3.47;
-          const rural_electricy = 0.97;
           let consume = number2 - number1;
-          let sub_total =
-            consume * unit_price + fixed_fee + public_light + meintainance;
+          let sub_total = consume * unit_price + fixed_fee + public_light + maintainance;
           let igv = sub_total * 0.18;
           let consume_total = (sub_total + igv + rural_electricy).toFixed(2);
           alert("S/ " + consume_total);
@@ -83,7 +144,12 @@ export default defineComponent({
     return {
       calculate,
       last_count,
-      current_count
+      current_count,
+      fixed_fee,
+      rural_electricy,
+      public_light,
+      maintainance,
+      unit_price
     }
   },
 })
