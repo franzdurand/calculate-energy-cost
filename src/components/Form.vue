c3c4ba74f3e9d1f76cdaed5d498369acfc2eb1cf
Franz Durand <franzdurand17@gmail.com>
2022-03-16 15:41:04 -0500

    Add validation

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,84 +1,101 @@
 <template>
   <div class="wrapper-mt-8 sm:mx-auto sm:w-full sm:max-w-md">
     <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-      <form class="space-y-6" @submit.prevent="calculate()">
+      <Form class="space-y-6" @submit="calculate" :validation-schema="schema">
         <div>
-          <label for="text" class="block text-sm font-medium text-gray-700">
+          <label for="unit_price" class="block text-sm font-medium text-gray-700">
             Unit Price
           </label>
           <div class="mt-1">
-            <input
-              v-model="unit_price"
+            <Field
               class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              id="unit_price"
+              name="unit_price"
+              value="0.6326"
+              />
+            <ErrorMessage name="unit_price" class="text-rose-500 text-xs" />
           </div>
         </div>
         <div>
-          <label for="text" class="block text-sm font-medium text-gray-700">
+          <label for="fixed_fee" class="block text-sm font-medium text-gray-700">
             Fixed fee
           </label>
           <div class="mt-1">
-            <input
-              v-model="fixed_fee"
+            <Field
               class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              id="fixed_fee"
+              name="fixed_fee"
+              value="2.61"
+              />
+            <ErrorMessage name="fixed_fee" class="text-rose-500 text-xs" />
           </div>
         </div>
         <div>
-          <label for="text" class="block text-sm font-medium text-gray-700">
+          <label for="maintainance" class="block text-sm font-medium text-gray-700">
             Maintainance
           </label>
           <div class="mt-1">
-            <input
-              v-model="maintainance"
+            <Field
               class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              id="maintainance"
+              name="maintainance"
+              value="1.32"
+              />
+            <ErrorMessage name="maintainance" class="text-rose-500 text-xs" />
           </div>
         </div>
         <div>
-          <label for="text" class="block text-sm font-medium text-gray-700">
+          <label for="public_light" class="block text-sm font-medium text-gray-700">
             Public light
           </label>
           <div class="mt-1">
-            <input
-              v-model="public_light"
+            <Field
               class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              id="public_light"
+              name="public_light"
+              value="3.47"
+              />
+            <ErrorMessage name="public_light" class="text-rose-500 text-xs" />
           </div>
         </div>
         <div>
-          <label for="text" class="block text-sm font-medium text-gray-700">
-            Rural electricity
+          <label for="rural_electricity" class="block text-sm font-medium text-gray-700">
+            Rural electricy
           </label>
           <div class="mt-1">
-            <input
-              v-model="rural_electricy"
+            <Field
               class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              id="rural_electricity"
+              name="rural_electricity"
+              value="0.97"
+              />
+            <ErrorMessage name="rural_electricity" class="text-rose-500 text-xs" />
           </div>
         </div>
         <div>
-          <label for="text" class="block text-sm font-medium text-gray-700">
+          <label for="last_count" class="block text-sm font-medium text-gray-700">
             Last count
           </label>
           <div class="mt-1">
-            <input
-              v-model="last_count"
-              placeholder="000000000"
+            <Field
               class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              id="last_count"
+              name="last_count"
+              />
+            <ErrorMessage name="last_count" class="text-rose-500 text-xs" />
           </div>
         </div>
         <div>
-          <label for="text" class="block text-sm font-medium text-gray-700">
+          <label for="current_count" class="block text-sm font-medium text-gray-700">
             Current count
           </label>
           <div class="mt-1">
-            <input
-              v-model="current_count"
-              placeholder="000000000"
+            <Field
               class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
+              id="current_count"
+              name="current_count"
+              />
+            <ErrorMessage name="current_count" class="text-rose-500 text-xs" />
           </div>
         </div>
         <div>
@@ -88,68 +105,63 @@
             Calculate
           </button>
         </div>
-      </form>
+      </Form>
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
 import { defineComponent } from '@vue/composition-api'
+import {Field, Form, ErrorMessage } from 'vee-validate'
+import * as Yup from "yup";
 
 export default defineComponent({
+  components: {
+    Field,
+    Form,
+    ErrorMessage,
+  },
   setup() {
-    let last_count = ref("");
-    let current_count = ref("");
-    let unit_price = ref(0.5006);
-    let fixed_fee = ref(2.61);
-    let maintainance = ref(1.32);
-    let public_light = ref(3.47);
-    let rural_electricy = ref(0.97);
 
-    const calculate = () => {
-      let number1 = last_count.value;
-      let number2 = current_count.value;
+    // Validate schema
+    const schema = Yup.object().shape({
+      fixed_fee: Yup.string().required().matches(/^[+-]?\d+(\.\d+)?$/, {message: "Insert valid number."}),
+      unit_price: Yup.string().required().matches(/^[+-]?\d+(\.\d+)?$/, {message: "Insert valid number."}),
+      maintainance: Yup.string().required().matches(/^[+-]?\d+(\.\d+)?$/, {message: "Insert valid number."}),
+      public_light: Yup.string().required().matches(/^[+-]?\d+(\.\d+)?$/, {message: "Insert valid number."}),
+      rural_electricity: Yup.string().required().matches(/^[+-]?\d+(\.\d+)?$/, {message: "Insert valid number."}),
+      last_count: Yup.string().required().matches(/^[+-]?\d+(\.\d+)?$/, {message: "Insert valid number."}),
+      current_count: Yup.string().required().matches(/^[+-]?\d+(\.\d+)?$/, {message: "Insert valid number."}),
+    });
+
+    function calculate(values) {
+      let number1 = values.last_count
+      let number2 = values.current_count
       
 
-      unit_price = +unit_price.value;
-      fixed_fee = +fixed_fee.value;
-      public_light = +public_light.value;
-      maintainance = +maintainance.value;
-      rural_electricy = +rural_electricy.value;
+      const unit_price = +values.unit_price
+      const fixed_fee = +values.fixed_fee
+      const public_light = +values.public_light
+      const maintainance = +values.maintainance
+      const rural_electricity = +values.rural_electricity
 
-      let n1 = +number1;
-      let n2 = +number2;
+      let n1 = +number1
+      let n2 = +number2
 
-      if (
-        (Number.isInteger(n1) || isFloat(n1)) &&
-        (isFloat(n2) || Number.isInteger(n2))
-      ) {
-        if (n1 > 0 && n2 > 0 && n2 > n1) {
-          let consume = number2 - number1;
-          let sub_total = consume * unit_price + fixed_fee + public_light + maintainance;
-          let igv = sub_total * 0.18;
-          let consume_total = (sub_total + igv + rural_electricy).toFixed(2);
-          alert("S/ " + consume_total);
-        } else {
-          alert("last count should be lower than current count");
-        }
+      if (n2 > n1) {
+        let consume = number2 - number1;
+        let sub_total = consume * unit_price + fixed_fee + public_light + maintainance;
+        let igv = sub_total * 0.18;
+        let consume_total = (sub_total + igv + rural_electricity).toFixed(2);
+        alert("S/ " + consume_total);
       } else {
-        alert("Please insert only numbers");
+        alert("last count should be lower than current count");
       }
     }
 
-    const isFloat = (n) => +n === n && n % 1 !== 0;
-
     return {
-      calculate,
-      last_count,
-      current_count,
-      fixed_fee,
-      rural_electricy,
-      public_light,
-      maintainance,
-      unit_price
+      schema,
+      calculate
     }
   },
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./dist/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
+        ...colors,
         primary: "#334874",
       },
     },
